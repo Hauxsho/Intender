540c09d157adf4f7a9437b7ff10fe573ddac60f7
hauxsho <hauxsho@gmail.com>
2020-05-15 19:06:59 +0530

    Photos xml

--- app/src/main/java/com/example/intender/Photos.java
+++ app/src/main/java/com/example/intender/Photos.java
@@ -2,16 +2,11 @@ package com.example.intender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 public class  Photos extends AppCompatActivity
 {
-
-    Button cont6;
-    TextView t1, t2, t3 , t4 ,t5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,23 +14,6 @@ public class  Photos extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
 
-        //Receive
-        Intent mintent = getIntent();
-        String phoneFI = mintent.getStringExtra("PHONEFR");
-        String nameFI= mintent.getStringExtra("NAMEFR");
-        String birthFI = mintent.getStringExtra("BIRTHDAYFR");
-        String genderFI = mintent.getStringExtra("GENDERFR");
-        String uniFI = mintent.getStringExtra("UNI");
 
-        t1=(TextView)findViewById(R.id.t1);
-        t1.setText(phoneFI);
-        t2=(TextView)findViewById(R.id.t2);
-        t2.setText(nameFI);
-        t3=(TextView)findViewById(R.id.t3);
-        t3.setText(birthFI);
-        t4=(TextView)findViewById(R.id.t4);
-        t4.setText(genderFI);
-        t5=(TextView)findViewById(R.id.t5);
-        t5.setText(uniFI);
     }
 }
